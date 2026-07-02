19e812a55572915603151500ba210cd3e5c50bf3
Exalloss <devinmtshafer@yahoo.com>
2026-07-01 21:32:11 -0400

    player and camera movement

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMin, lockVertMax;
+
+    float camRotX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sens;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sens;
+
+        camRotX -= mouseY;
+        camRotX = Mathf.Clamp(camRotX, lockVertMin, lockVertMax);
+        transform.localRotation = Quaternion.Euler(camRotX, 0, 0);
+
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int jumpMax;
+    [SerializeField] int gravity;
+
+    int jumpCount;
+    int HPOriginal;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+     
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HPOriginal = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            playerVel.y = 0;
+            jumpCount = 0;
+        }
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            playerVel.y = jumpSpeed;
+            jumpCount++;
+        }
+    }
+}
