d8139da76b4bbb8c867298f27b0c698d126a2cc4
Samuel Herman <frydthundor@gmail.com>
2026-07-03 16:43:02 -0400

    Added game and time manager
    pushing to get jump feature from Virel

--- Assets/Scripts/timeManager.cs
+++ Assets/Scripts/timeManager.cs
@@ -4,16 +4,24 @@ public class timeManager : MonoBehaviour
 {
 
     [SerializeField] float defaultTimeScale;
+    [SerializeField] float minTimeScale;
+    [SerializeField] float currentTimeScale;
+
+    [SerializeField] bool update;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Time.timeScale = defaultTimeScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (update)
+        {
+            Time.timeScale = currentTimeScale;
+            update = false;
+        }
     }
 }
