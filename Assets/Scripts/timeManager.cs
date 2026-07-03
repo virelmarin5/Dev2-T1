121d6761a8c4e28e50f897e52aebccc07560ca66
Samuel Herman <frydthundor@gmail.com>
2026-07-03 16:18:34 -0400

    Added building

---
+++ Assets/Scripts/timeManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class timeManager : MonoBehaviour
+{
+
+    [SerializeField] float defaultTimeScale;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
