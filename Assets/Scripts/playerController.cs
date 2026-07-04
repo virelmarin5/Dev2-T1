de4969c605c2b5134ae4eeb449294f7ed687b9da
Khurshed <158274131+rizvi98@users.noreply.github.com>
2026-07-04 15:13:46 -0400

    Pull remaining files from khurshed

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class playerController : MonoBehaviour, IDamage, IPickGun
 {
     [SerializeField] CharacterController controller;
 
@@ -10,6 +11,11 @@ public class playerController : MonoBehaviour
     [SerializeField] int jumpSpeed;
     [SerializeField] int jumpMax;
     [SerializeField] int gravity;
+
+    [SerializeField] gunStatsHandler currGun;
+    [SerializeField] GameObject gunHoldPos;
+
+    float shootTimer;
 
     int jumpCount;
     int HPOriginal;
@@ -45,6 +51,14 @@ public class playerController : MonoBehaviour
 
         controller.Move(playerVel * Time.deltaTime);
         playerVel.y -= gravity * Time.deltaTime;
+
+        shootTimer += Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1") && currGun != null && shootTimer > currGun.shootRate)
+            shoot();
+
+        if(currGun != null)
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * currGun.shootDist, Color.red);
     }
 
     void jump()
@@ -54,5 +68,62 @@ public class playerController : MonoBehaviour
             playerVel.y = jumpSpeed;
             jumpCount++;
         }
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+        if (HP <= 0)
+        {
+            // dead
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, currGun.shootDist))
+        {
+            IDamage dmg = hit.transform.GetComponent<IDamage>();
+            if (dmg != null)
+                dmg.takeDamage(currGun.shootDamage);
+        }
+    }
+
+    public void gunStatsHandler(gunStatsHandler gun)
+    {
+        // Replace the current gun with the newly picked up one
+        currGun = gun;
+        changeGun();
+    }
+
+    void changeGun()
+    {
+        if (gunHoldPos == null) return;
+
+        // 1. Destroy whatever gun the player is currently holding
+        foreach (Transform child in gunHoldPos.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // 2. If we just dropped/cleared the weapon slot, stop here
+        if (currGun == null || currGun.gunModel == null) return;
+
+        // 3. Spawn the entire modular weapon prefab as a child of our gun holder
+        GameObject newWeapon = Instantiate(currGun.gunModel, gunHoldPos.transform);
+
+        // 4. Reset its transform so it snaps perfectly to the player's hands
+        newWeapon.transform.localPosition = Vector3.zero;
+        newWeapon.transform.localRotation = Quaternion.identity;
+        newWeapon.transform.localScale = Vector3.one;
+
+        // 5.Turn off colliders and trigger scripts on the held version
+        // So the player doesn't accidentally trigger a pickup on the gun they are holding
+        if (newWeapon.TryGetComponent<Collider>(out Collider col)) col.enabled = false;
+        if (newWeapon.TryGetComponent<pickGun>(out pickGun script)) script.enabled = false;
     }
 }
