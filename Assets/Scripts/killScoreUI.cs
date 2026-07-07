c5b87405272dd8727e2f904f137fb63e4118fa8e
Mark <fittantec21@gmail.com>
2026-07-07 16:21:34 -0600

    added kill tracker & wave tracker UI

---
+++ Assets/Scripts/killScoreUI.cs
@@ -0,0 +1,68 @@
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+/*
+ * created by Mark Fittante
+ * 
+ * Connects to any script that results in enmey death
+ * 
+ * Depending on which enemy will show different scores
+ * 
+ * 
+ */
+
+
+
+
+
+public class killScoreUI : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI killCounter;
+    [SerializeField] TextMeshProUGUI waveCounter;
+
+    int currentKill;
+    int currentScore;
+    int enemiesAlive;
+    int previousEnemiesAlive;
+    int previousWave;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+        currentKill = 0;
+        currentScore = 0;
+
+        previousEnemiesAlive = waveManager.instance.getEnemiesAlive();
+        previousWave = waveManager.instance.getCurrentWave();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentWave = waveManager.instance.getCurrentWave();
+        enemiesAlive = waveManager.instance.getEnemiesAlive();
+
+        if (currentWave != previousWave)
+        {
+            previousWave = currentWave;
+            previousEnemiesAlive = enemiesAlive;
+        }
+        else if (enemiesAlive < previousEnemiesAlive)
+        {
+            currentKill += previousEnemiesAlive - enemiesAlive;
+        }
+
+        previousEnemiesAlive = enemiesAlive;
+
+        killCounter.text = "Kills: " + currentKill;
+        waveCounter.text = "Wave: " + currentWave;
+
+    }
+
+
+   
+
+}
