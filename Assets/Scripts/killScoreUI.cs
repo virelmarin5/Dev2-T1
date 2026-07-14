d4616476c422be7190c5ab87f6eb9f126c1f6283
Mark Fittante <fittantec21@gmail.com>
2026-07-14 14:49:07 -0600

    Changed color of Kill streak and added effect to wave counter

--- Assets/Scripts/killScoreUI.cs
+++ Assets/Scripts/killScoreUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -21,6 +22,11 @@ public class killScoreUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI killCounter;
     [SerializeField] TextMeshProUGUI waveCounter;
     [SerializeField] TextMeshProUGUI pauseKills;
+
+    [Header("UI Effects")]
+    [SerializeField] float fadeTime = 0.75f;
+    [SerializeField] float displayTime = 2f;
+    [SerializeField] ParticleSystem burstEffect;
 
     int currentKill;
     //int currentScore;
@@ -49,6 +55,8 @@ public class killScoreUI : MonoBehaviour
         {
             previousWave = currentWave;
             previousEnemiesAlive = enemiesAlive;
+
+            StartCoroutine(waveEffects(currentWave));
         }
         else if (enemiesAlive < previousEnemiesAlive)
         {
@@ -59,7 +67,42 @@ public class killScoreUI : MonoBehaviour
 
         killCounter.text = "Kills: " + currentKill;
         pauseKills.text = "Kills: " + currentKill;
-        waveCounter.text = "Wave: " + currentWave;
+        //waveCounter.text = "Wave: " + currentWave;
+    }
+
+    IEnumerator waveEffects(int wave)
+    {
+        if (burstEffect != null)
+        {
+            burstEffect.Play();
+        }
+
+        waveCounter.text = $"Wave {wave}";
+
+        Color c = waveCounter.color;
+        c.a = 0f;
+        waveCounter.color = c;
+
+        waveCounter.transform.localScale = Vector3.one * 0.6f;
+
+        float timer = 0f;
+
+        while (timer < fadeTime)
+        {
+            timer+= Time.deltaTime;
+            float t = timer / fadeTime;
+
+            c.a = Mathf.Lerp(0f,1f,t);
+            waveCounter.color = c;
+
+            waveCounter.transform.localScale = Vector3.Lerp(Vector3.one * 0.6f, Vector3.one, t);
+
+            yield return null;
+        }
+
+        c.a = 1f;
+        waveCounter.color = c;
+        
     }
 
     public int getKillCount()
